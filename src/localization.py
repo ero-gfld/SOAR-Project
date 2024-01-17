c022685d24b4daee231161464fffe8cfb568763a
eroussin <evan.roussin@gmail.com>
2024-01-17 22:33:27 +0100

    Added kNN

--- src/localization.py
+++ src/localization.py
@@ -6,12 +6,26 @@ from nav_msgs.srv import GetMap
 from nav_msgs.msg import OccupancyGrid
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn import neighbors
 
 import numpy as np
+
+import re
 
 import copy
 import time
 import math
+
+# Define colour scheme
+colourScheme = {
+    "darkblue": "#143049",
+    "twblue": "#00649C",
+    "lightblue": "#8DA3B3",
+    "lightgrey": "#CBC0D5",
+    "twgrey": "#72777A"
+}
 
 # Helper method for retrieving the map
 def getMap() -> OccupancyGrid:
@@ -27,15 +41,6 @@ def getMap() -> OccupancyGrid:
 # Helper method to show the map
 def showMap(freePositions, wallPositions, scanPositions):
     """ Displays map """
-    # Store colours matching UAS TW colour scheme as dict 
-    colourScheme = {
-        "darkblue": "#143049",
-        "twblue": "#00649C",
-        "lightblue": "#8DA3B3",
-        "lightgrey": "#CBC0D5",
-        "twgrey": "#72777A"
-    }
-
     ## Visualise transformed maze and scans
     # Create single figure
     plt.rcParams['figure.figsize'] = [7, 7]
@@ -65,9 +70,69 @@ def showMap(freePositions, wallPositions, scanPositions):
 
     # Show plot
     plt.show()
+
+def visualiseClf(clf, X):
+    # Visualise fitted model using built-in visualiser
+    plt.rcParams['figure.figsize'] = [5, 5]
+    _, ax = plt.subplots()
+
+    DecisionBoundaryDisplay.from_estimator(
+        clf,
+        X,
+        cmap=ListedColormap(
+            [
+                colourScheme["twgrey"],
+                colourScheme["darkblue"]
+            ]
+        ),
+        ax=ax,
+        response_method="predict",
+        plot_method="pcolormesh",
+        xlabel="X-Coordinate[m]",
+        ylabel="Y-Coordinate[m]",
+        shading="auto",
+    )
+
+    # Set title and show plot
+    ax.set_title("Fitted kNN Model representing the Map")
+    plt.show()
+
+# Helper function for colours
+# See https://matplotlib.org/stable/tutorials/colors/colormaps.html for more information
+def createColourmapToWhite(rgb, reverse=False):
+    """ Creates a colourmap that fades from rgb colour to white """
+    # Unpack tuple and manually create fades with a resolution of 256
+    r, g, b = rgb
+    N = 256
+    vals = np.ones((N, 4))
+    # Distinguish between reverse and non-reverse and invert linspace accordingly
+    if reverse:
+        vals[:, 0] = np.linspace(1, r/N, N)
+        vals[:, 1] = np.linspace(1, g/N, N)
+        vals[:, 2] = np.linspace(1, b/N, N)
+    else:
+        vals[:, 0] = np.linspace(r/N, 1, N)
+        vals[:, 1] = np.linspace(g/N, 1, N)
+        vals[:, 2] = np.linspace(b/N, 1, N)
+    return ListedColormap(vals)
+
+def hexstring2rgb(colourstring):
+    """ Converts hex colours in string form to rgb values """
+    i = re.compile('#')
+    colourstring = re.sub(i, '', colourstring)
+    return tuple(int(colourstring[i:i+2], 16) for i in (0, 2, 4))
+
+# Create colourmap that fades from white to UAS TW blue for heatmap
+heatColourMap = createColourmapToWhite( hexstring2rgb( colourScheme["twblue"] ), reverse=True)
+
+# ----------------------------------------------------------------
     
 # Initiate ROS node
 rospy.init_node('localization')
+
+# ----------------------------------
+# Step 1: Get map and laserscan data
+# ----------------------------------
 
 # Wait until the node exists else it will throw an error
 rospy.wait_for_service('static_map')
@@ -114,4 +179,55 @@ scanPositions = scanPositions + (recMap.info.resolution / 2)
 scanPositions = np.reshape(scanPositions, (-1, 2))
 
 # Show the map of the scan
-showMap(freePositions, wallPositions, scanPositions)
+# showMap(freePositions, wallPositions, scanPositions)
+
+# ---------------------------------
+# Step 2: Localisation using kNN
+# ---------------------------------
+
+print("Start predicting...")
+
+# Use KNN to find the nearest neighbour for each point in the scan
+knn = neighbors.KNeighborsClassifier(n_neighbors=1)
+
+# X will be all the coordinates of the map's points and Y will be the class of the point (0 for free, 1 for wall)
+X = np.concatenate((wallPositions, freePositions))
+Y = np.concatenate((np.ones(len(wallPositions)), np.zeros(len(freePositions))))
+
+# Fit the model
+clf = knn.fit(X, Y)
+
+# Visualise the model
+# visualiseClf(clf, X)
+
+# Define all possible poses (positions where no walls and coordinates that are whole numbers)
+poses = []
+for x in range(int(recMap.info.origin.position.x), int(recMap.info.origin.position.x + recMap.info.width * recMap.info.resolution) + 1, 1):
+    for y in range(int(recMap.info.origin.position.y), int(recMap.info.origin.position.y + recMap.info.height * recMap.info.resolution) + 1, 1):
+        if [x, y] not in wallPositions.tolist():
+            poses.append([x, y])
+
+# Iterate over all poses
+allPredictions = {}
+for pose in poses:
+    # Transform the laserscan to the current pose
+    transformedScan = scanPositions + pose
+    
+    # Predict the values using the fitted model
+    predictions = clf.predict(transformedScan)
+    
+    # Count the number of "wall" predictions
+    wallCount = np.count_nonzero(predictions)
+
+    # Store the number of "wall" predictions for each pose
+    allPredictions[tuple(pose)] = wallCount
+
+print("Finished predicting.")
+
+# Remove prediction with 0 walls
+allPredictions = {k: v for k, v in allPredictions.items() if v != 0}
+
+# Print all predictions in descending order
+print("All predictions in descending order:")
+for key, value in sorted(allPredictions.items(), key=lambda item: item[1], reverse=True):
+    print("%s: %s" % (key, value))
