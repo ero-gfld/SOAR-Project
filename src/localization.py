1d83a8d7374434f405a008235b24ca17d633bd13
eroussin <evan.roussin@gmail.com>
2024-01-17 16:28:02 +0100

    Successfully added the scan to the map and fixed the positions

--- src/localization.py
+++ src/localization.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-# Import neccesary modules
-# Set matplotlib to inline mode for the Jupyter notebook visualisations
-
-# %matplotlib inline
-
 import rospy
 from sensor_msgs.msg import LaserScan
 from nav_msgs.srv import GetMap
@@ -30,7 +25,7 @@ def getMap() -> OccupancyGrid:
     return recMap
 
 # Helper method to show the map
-def showMap(data):
+def showMap(freePositions, wallPositions, scanPositions):
     """ Displays map """
     # Store colours matching UAS TW colour scheme as dict 
     colourScheme = {
@@ -46,21 +41,8 @@ def showMap(data):
     plt.rcParams['figure.figsize'] = [7, 7]
     fig, ax = plt.subplots()
 
-    # Get data into wallPosition and freePosition arrays
-    wallPositions = np.array([])
-    freePositions = np.array([])
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            if data[i][j] == 100:
-                wallPositions = np.append(wallPositions, [i, j])
-            elif data[i][j] == 0:
-                freePositions = np.append(freePositions, [i, j])
-
-    # Reshape arrays to be 2D
-    wallPositions = np.reshape(wallPositions, (-1, 2))
-    freePositions = np.reshape(freePositions, (-1, 2))
-
     # Plot data as points (=scatterplot) and label accordingly. The colours are to look nice with UAS TW colours
+    ax.scatter(scanPositions[:,1], scanPositions[:,0], c="r", alpha=0.8, label="Laserscan")
     ax.scatter(wallPositions[:,1], wallPositions[:,0], c=colourScheme["darkblue"], alpha=1.0, s=6**2, label="Walls")
     ax.scatter(freePositions[:,1], freePositions[:,0], c=colourScheme["twgrey"], alpha=0.08, s=6**2, label="Unobstructed Space")
     ax.scatter([0], [0], c=colourScheme["twblue"], s=15**2, label="Scan Center")
@@ -93,8 +75,43 @@ rospy.wait_for_service('static_map')
 # Get the map
 recMap = getMap()
 
-# Deserialise into a 2D array
+# Get the map data
 mapData = np.split(np.array(recMap.data), recMap.info.height)
 
-# Show the map
-showMap(mapData)
+# Get data into wallPosition and freePosition arrays
+wallPositions = np.array([])
+freePositions = np.array([])
+for i in range(len(mapData)):
+    for j in range(len(mapData[i])):
+        x = i * recMap.info.resolution + (recMap.info.origin.position.x + recMap.info.resolution / 2)
+        y = j * recMap.info.resolution + (recMap.info.origin.position.y + recMap.info.resolution / 2)
+        if mapData[i][j] == 100:
+            wallPositions = np.append(wallPositions, [x, y])
+        elif mapData[i][j] == 0:
+            freePositions = np.append(freePositions, [x, y])
+
+# Reshape arrays to be 2D
+wallPositions = np.reshape(wallPositions, (-1, 2))
+freePositions = np.reshape(freePositions, (-1, 2))
+
+# Get the laser scan data
+scanData = rospy.wait_for_message('/scan', LaserScan)
+
+# Use np.arange to get the angles of each measurement
+angles = np.arange(scanData.angle_min, scanData.angle_max, scanData.angle_increment)
+
+# Convert angle-distance pairs to Cartesian coordinates
+scanPositions = np.array([])
+for i in range(len(angles)):
+    x = scanData.ranges[i]*math.sin(angles[i])
+    y = scanData.ranges[i]*math.cos(angles[i])
+    scanPositions = np.append(scanPositions, [x, y])
+
+# The walls having thickness laserscan and walls won't match up perfectly. We move each point away from the laserscan by half the map's resolution.
+scanPositions = scanPositions + (recMap.info.resolution / 2)
+
+# Reshape array to be 2D
+scanPositions = np.reshape(scanPositions, (-1, 2))
+
+# Show the map of the scan
+showMap(freePositions, wallPositions, scanPositions)
